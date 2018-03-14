3dcb5a6196a32d21ab834623cf0557d92ded1b5a
Pasever <pas1034ever@gmail.com>
2018-03-14 00:03:18 -0400

    a.0.2

--- schema/schema.js
+++ schema/schema.js
@@ -1,10 +1,17 @@
 const graphql = require('graphql');
+const _ = require('lodash');
 const {
     GraphQLObjectType,
     GraphQLString,
-    GraphQLInt   
+    GraphQLInt ,
+    GraphQLSchema  
 } = graphql;
 
+
+const users = [
+    { id: '23',  firstName: "Will", age: 25 },
+    { id: '55',  firstName: "Amantha", age: 31 }
+];
 const UserType = new GraphQLObjectType({
     name: 'User',
     fields: {
@@ -21,8 +28,13 @@ const RootQuery = new GraphQLObjectType({
             type: UserType,
             args: {id: { type: GraphQLString } },
             resolve(parentValue, args) {
+                return _.find(users, { id: args.id });
                 
             }
         }
     }
+});
+
+module.exports = new GraphQLSchema({
+    query: RootQuery
 });
--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
 const expressGraphQL = require('express-graphql');
+const schema = require('./schema/schema');
 
 app.use('/graphql', expressGraphQL({
+    schema,
     graphiql: true    //development tool for queries against out development server (only for dev env)
 }));
 
